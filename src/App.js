a2909320928d72184077708cd5503e3772c1c158
janhavi544 <73301992+janhavi544@users.noreply.github.com>
2022-12-21 18:49:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState ,useEffect} from 'react';
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Response from "./response";
+import Home from "./Home";
+
+
+function App() {
+
+  useEffect(()=>{
+  },[])
+
+  return (
+    //for two screens:one for the form other for the payment successful screen
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Response" element={<Response/>}/>
+      </Routes>
+    {/* <div style={{ height: "200px" }}></div> */}
+  </div>
+  );
+}
+
+const AppWrapper = () => {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+};
+
+export default AppWrapper;
---
+++ src/response.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Response.css';
+function Response(){
+  return (
+    <div className="ResponseScreen">
+      <br></br>
+      <h2 >Payment succesful!!</h2>
+    </div>
+  )
+}
+
+export default Response;
